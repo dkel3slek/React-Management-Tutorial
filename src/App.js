0cdd5c6335d1a207a9f33b1bd4f1834f29c16d99
dkel3slek <dkel3slek@naver.com>
2020-10-16 01:37:25 +0900

    Create Customer.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,46 @@
 import React from 'react';
 import './App.css';
+import Customer from './components/customer'
+
+
+const customers = [
+  {
+    'id': 1,
+    'image': 'https://placeimg.com/64/64/1',
+    'name': '이선우',
+    'birthday': '960406',
+    'gender': '남자',
+    'job': '대학생'
+  },
+  {
+    'id': 2,
+    'image': 'https://placeimg.com/64/64/2',
+    'name': '김소연',
+    'birthday': '960214',
+    'gender': '여자',
+    'job': '바보'
+  },
+  {
+    'id': 3,
+    'image': 'https://placeimg.com/64/64/3',
+    'name': '정승규',
+    'birthday': '961002',
+    'gender': '남자',
+    'job': '병신'
+  }
+]
 
 class App extends React.Component{
-  constructor(props){
-    super(props)
-    this.state = {toggle: true}
-    this.change = this.change.bind(this)
-  }
-  change(){
-    this.setState({toggle: !this.state.toggle})
-  }
   render(){
-    return <button id="onOff" onClick={this.change}>{this.state.toggle ? 'on' : 'off'}</button>
+    return(
+      <div>
+        {
+          customers.map(v => {
+            return <Customer key={v.id} id={v.id} image={v.image} name={v.name} birthday={v.birthday} gender={v.gender} job={v.job}/>
+          })
+        }
+      </div>
+    )
   }
 }
 
